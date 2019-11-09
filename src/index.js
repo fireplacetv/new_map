67cd7abe5d648fed14419acd52212cc3143519fc
fireplacetv <derrick.low@getaround.com>
2019-11-09 11:18:05 -0800

    forgot to add files

--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ import settings from "./settings.json";
 import custom from "./custom-style.json";
 
 let map;
+let hovered;
+const popup = document.querySelector("#popup");
 
 async function init() {
-    const neighborhoods = await import("../data/la-county-neighborhoods-v5.json");
-    const sites = await import("../data/sites.json");
+    const output = await import("../data/output.json");
     const style = map.getStyle();
 
     style.sources = {
@@ -18,9 +19,41 @@ async function init() {
     style.layers.push(...custom.layers);
     map.setStyle(style);
 
-    map.getSource("neighborhoods").setData(neighborhoods);
-    map.getSource("sites").setData(sites);
+    map.getSource("output").setData(output);
+    initPopup();
 }
+
+function initPopup() {
+    const nameEl = popup.querySelector(".name");
+    const countEl = popup.querySelector(".count");
+
+    map.on("mousemove","output-floor",function(e) {
+      clearHover();
+
+      if (e.features.length > 0) {
+          hovered = e.features[0];
+          map.setFeatureState(hovered, {
+              hover: true
+          });
+          popup.style.display = "block";
+          nameEl.textContent = hovered.properties.name;
+          countEl.textContent = hovered.properties.count;
+      }
+    });
+
+    map.on("mouseleave", "output-floor", clearHover);
+}
+
+function clearHover() {
+  if (hovered) {
+    map.setFeatureState(hovered, {
+        hover: false
+    });
+    hovered = null;
+    popup.style.display = "none";
+  }
+}
+
 
 mapboxgl.accessToken = settings.accessToken;
 map = new mapboxgl.Map(settings);
--- src/process.mjs
+++ src/process.mjs
@@ -1,1 +1,29 @@
-console.log("success. 👍");
+import fs from "fs";
+import turf from "@turf/turf"
+
+import neighborhoods from "../data/la-county-neighborhoods-v5.json";
+import sites from "../data/sites.json";
+
+// iterate over sites points to create a property & set it to 1 for turf
+sites.features.forEach(function(feature) {
+    feature.properties = {
+        count: 1
+    };
+});
+
+let output = turf.collect(neighborhoods, sites, 'count', 'count');
+
+output.features = output.features.filter(function(feature, i) {
+    feature.id = i;
+    feature.properties.count = feature.properties.count.length;
+    return (feature.properties.count > 0);
+});
+
+output = JSON.stringify(output, null, "\t");
+fs.writeFile("../data/output.json", output, function(err) {
+    if (err) {
+        throw err;
+    }
+
+    console.log("success. 👍");
+});
